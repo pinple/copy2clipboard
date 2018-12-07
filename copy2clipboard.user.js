67772c1c553b1dcf74fbaf341471648074c54514
Neulana <fupinyou@gmail.com>
2018-12-07 18:25:21 +0800

    🐛 fix <code> tag check

--- copy2clipboard.user.js
+++ copy2clipboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         copy2clipboard
 // @namespace    https://github.com/Neulana/copy2clipboard
-// @version      0.1
+// @version      0.2
 // @description  a tampermonkey to copy code from stackoverflow.com etc.
 // @author       Neulana; https://github.com/Neulana
 // @include      https://github.com/*
@@ -9,6 +9,7 @@
 // @include      https://*.zhihu.com/*
 // @include      https://www.jianshu.com/*
 // @include      https://dev.to/*
+// @include      *.github.to/*
 // @require      https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
 // @grant        none
 // @license      Apache-2.0
@@ -75,7 +76,7 @@
             "-webkit-border-radius": "5px",
             "border": "2px solid #CCCCCC"
         });
-        var $codeContainer = $copyCodeButton.siblings("code");
+
         setTimeout(function () {
             if ($codeContainer.length == 0) {
                 $(pre).contents().filter(function () {
@@ -111,6 +112,7 @@
         });
 
         $(this).append($copyCodeButton);
+        var $codeContainer = $copyCodeButton.siblings("code");
         $("pre").hover(function () {
             $(this).children(".copy-code-button").css("display", "block");
         }, function () {
